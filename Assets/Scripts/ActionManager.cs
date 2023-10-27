0d517b6a18e75eaa64e20bcbfe452b91da80a0b5
Samuel Rantatalo <samuel.miska.rantatalo@gmail.com>
2023-10-27 22:31:37 +0300

    Kaiken alku ja juuri

---
+++ Assets/Scripts/ActionManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionManager : MonoBehaviour
+{
+    //Find empty positions
+    //Player action
+    //If player swaps position:
+    //-Remove oldPosition from empty positions
+    //-Add current position to empty positions
+    //Set target position
+    //Remove Target position from empty positions
+    //Rotate towards TargetPosition
+    //Move to TargetPosition
+
+    [SerializeField] List<Vector3> emptyPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindEmptyPositions();   
+    }
+
+    void FindEmptyPositions()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RoundedPosition(Vector3 _position)
+    {
+        Vector3 roundedPosition = new Vector3(Mathf.Round(_position.x), 0, Mathf.Round(_position.z));
+        return roundedPosition;
+    }
+}
